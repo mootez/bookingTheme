cb0b0da64f671fd973bc9fdb03084377db43a9ff
mootez <klaimaotez@gmail.com>
2018-10-28 15:50:34 +0100

    gallery

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OffresComponent }      from './offres/offres.component';
 import { HomeComponent }      from './home/home.component';
+import { VoyagerComponent }      from './voyager/voyager.component';
 
 
 const routes: Routes = [
   { path: 'offres', component: OffresComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'voyager', component: VoyagerComponent }
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { OffresComponent } from './offres/offres.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { HomeComponent } from './home/home.component';
+import { VoyagerComponent } from './voyager/voyager.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OffresComponent,
-    HomeComponent
+    HomeComponent,
+    VoyagerComponent
   ],
   imports: [
     BrowserModule,
